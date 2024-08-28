7ad27e7eaee434d54df9cf64500774da2eeca782
SmallTeddy <zhangzeming1212@gmail.com>
2024-08-28 11:15:11 +0800

    fix: bag panel data

--- src/views/Game/Panel/BagPanel.vue
+++ src/views/Game/Panel/BagPanel.vue
@@ -1,47 +1,43 @@
 <script lang="ts" setup>
-const tabs = ['装备', '药品', '宠物']
-const activeTab = ref('装备')
+const tabsItems = [
+  {
+    name: "装备",
+    data: [{ name: "111" }, { name: "222" }],
+  },
+  {
+    name: "药品",
+    data: [{ name: "111" }, { name: "222" }, { name: "333" }, { name: "444" }],
+  },
+  {
+    name: "宠物",
+    data: [{ name: "111" }, { name: "222" }, { name: "333" }],
+  },
+];
 
-// 装备
-const equipmentItems = [
-  { name: '111' },
-  { name: '222' },
-  { name: '333' },
-  { name: '444' },
-  { name: '555' },
-  { name: '666' },
-  { name: '777' },
-  { name: '888' },
-  { name: '999' },
-  { name: '000' },
-]
+const activeTab = ref("装备");
 
-const medicineItems = [
-  { name: '111' },
-  { name: '222' },
-  { name: '333' }
-]
-
-const petItems = [
-  { name: '111' },
-  { name: '222' },
-]
+const dataItems = computed(() => {
+  return tabsItems.find((item) => item.name === activeTab.value)?.data || [];
+});
 </script>
 
 <template>
   <div hw-full border-dashed-base>
     <div flex>
-      <label v-for="label in tabs" h-52px w-70px flex-center cursor-pointer @click="activeTab = label">{{ label }}</label>
+      <label
+        v-for="item in tabsItems"
+        h-52px
+        w-70px
+        flex-center
+        cursor-pointer
+        @click="activeTab = item.name"
+      >
+        {{ item.name }}
+      </label>
     </div>
     <div>
-      <div v-show="activeTab === '装备'" hw-full flex gap-2 p2>
-        <div v-for="equipment in equipmentItems" equipment-box border-dashed-base></div>
-      </div>
-      <div v-show="activeTab === '药品'" hw-full flex gap-2 p2>
-        <div v-for="equipment in medicineItems" equipment-box border-dashed-base></div>
-      </div>
-      <div v-show="activeTab === '宠物'" hw-full flex gap-2 p2>
-        <div v-for="equipment in petItems" equipment-box border-dashed-base></div>
+      <div hw-full flex gap-2 p2>
+        <div v-for="item in dataItems" equipment-box border-dashed-base></div>
       </div>
     </div>
   </div>
