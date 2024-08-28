21d6d9f249a1f073279c0497014aa928f7f62d91
SmallTeddy <zhangzeming1212@gmail.com>
2024-08-28 11:01:40 +0800

    feat: add bag component

--- src/views/Game/Panel/BagPanel.vue
+++ src/views/Game/Panel/BagPanel.vue
@@ -1,7 +1,50 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const tabs = ['装备', '药品', '宠物']
+const activeTab = ref('装备')
+
+// 装备
+const equipmentItems = [
+  { name: '111' },
+  { name: '222' },
+  { name: '333' },
+  { name: '444' },
+  { name: '555' },
+  { name: '666' },
+  { name: '777' },
+  { name: '888' },
+  { name: '999' },
+  { name: '000' },
+]
+
+const medicineItems = [
+  { name: '111' },
+  { name: '222' },
+  { name: '333' }
+]
+
+const petItems = [
+  { name: '111' },
+  { name: '222' },
+]
+</script>
 
 <template>
-  <div hw-full flex-center border-dashed-base>背包面板</div>
+  <div hw-full border-dashed-base>
+    <div flex>
+      <label v-for="label in tabs" h-52px w-70px flex-center cursor-pointer @click="activeTab = label">{{ label }}</label>
+    </div>
+    <div>
+      <div v-show="activeTab === '装备'" hw-full flex gap-2 p2>
+        <div v-for="equipment in equipmentItems" equipment-box border-dashed-base></div>
+      </div>
+      <div v-show="activeTab === '药品'" hw-full flex gap-2 p2>
+        <div v-for="equipment in medicineItems" equipment-box border-dashed-base></div>
+      </div>
+      <div v-show="activeTab === '宠物'" hw-full flex gap-2 p2>
+        <div v-for="equipment in petItems" equipment-box border-dashed-base></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="less"></style>
